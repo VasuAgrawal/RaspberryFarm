d061ace3891b91552d7ced7680c4f62ef48d7a65
Vasu Agrawal <vasu_agrawal@yahoo.com>
2016-01-14 00:22:56 -0500

    Finished up the login script. This cycles through a number of ports and tries to login on all of them. After logging it, it cleanly closes the connection. The net result is that we are logged in to all of the raspberry pis, ready to accept commands.

--- login.py
+++ login.py
@@ -1,6 +1,41 @@
 import pexpect
+from pexpect import fdpexpect
+import fuckit
+import os
 
-child = pexpect.spawn("screen /dev/ttyUSB1 115200")
-child.sendline("pi")
-child.sendline("raspberry")
-child.interact()
+def login(port, user="pi", password="raspberry", baudrate=115200):
+    timeout = 5
+
+    # child = pexpect.spawn("screen %s %d" % (port, baudrate))
+    child = fdpexpect.fdspawn(os.open(port, os.O_RDWR | os.O_NONBLOCK |
+        os.O_NOCTTY))
+
+    # Needed to start the whole buffering thing, and to be able to see output on
+    # the screen
+    child.sendline("")
+
+    # We see if we got the bash prompt instead of the login prompt. If we get
+    # the bash prompt, we're already logged in.
+    with fuckit:
+        child.expect("%s@raspberrypi" % user, timeout=timeout)
+        print "Already logged in, returning!"
+        return child
+
+    # try to log in 3 times
+    for i in range(3):
+        try:
+            child.expect("raspberrypi login:", timeout=timeout)
+            child.sendline(user)
+            child.expect("Password:", timeout=timeout)
+            child.sendline(password)
+            return child
+        except:
+            print "Failed to log in, trying again ..."
+            continue
+
+    raise Exception("Something is probably wrong with the raspberry pi?")
+
+for i in range(1, 4, 2):
+    print "Logging into /dev/ttyUSB%d" % i
+    login("/dev/ttyUSB%d" % i).close()
+
